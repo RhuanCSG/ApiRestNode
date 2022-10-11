d7e08458a87a7f6ca6b71010f3d9aad15c4f8cf0
Rhuan Guimarães <rhuan.guimaraes@ecustomize.local>
2022-10-11 14:15:07 -0300

    Project created and configured

---
+++ app.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+import express from 'express';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+
+  }
+}
+
+export default new App().app;
